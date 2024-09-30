0d068f4cdd05c934ae1e907a33891dca66d94aca
Zach Shelly <zshelly2000@gmail.com>
2024-09-30 11:00:31 -0700

    continually trying to improve the file directory situation by utilizing the here package

--- main/main.R
+++ main/main.R
@@ -3,16 +3,18 @@ library(tidyverse)
 library(dplyr)
 library(httr)
 library(jsonlite)
+library(here)
 
-# Set the working directory to the project root
-setwd("..") # Moves up one directory form 'main/ to 'repo/'
+rprojroot()
 
 # Source the scripts
-source("data_gathering/Accessing_NBA_Data_via_an_API.R")
-source("data_gathering/Web_Scrape_Data_From_Basketball_reference_advanced.R")
-source("data_gathering/Web_Scrape_Data_From_Basketball_reference_per_game.R")
+source(here("data_gathering", "Accessing_NBA_Data_via_an_API.R"))
+source(here("data_gathering", "Web_Scrape_Data_From_Basketball_reference_advanced.R"))
+source(here("data_gathering", "Web_Scrape_Data_From_Basketball_reference_per_game.R"))
 
 # Call the functions
 nba_stats <- get_NBA_data_via_API()
 br_advanced <- advanced_br_webscrape()
 br_per_game <- per_game_br_webscrape()
+
+print(getwd())
