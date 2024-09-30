8479398d700afd6d7a7982bfefc65bcc12fb4027
Zach Shelly <zshelly2000@gmail.com>
2024-09-29 17:20:17 -0700

    I copied and pasted the first br webscrape process with a different url. During that process, I realized that the I didn't remove the repeated column headers so I added that function to the first script as well.

--- Web_Scrape_Data_From_Basketball_reference_advanced.R
+++ Web_Scrape_Data_From_Basketball_reference_advanced.R
@@ -4,7 +4,7 @@ library(dplyr)
 
 
 # Specify the URL
-url <- paste0("https://www.basketball-reference.com/leagues/NBA_2024_per_game.html")
+url <- paste0("https://www.basketball-reference.com/leagues/NBA_2024_advanced.html")
 
 # Read the HTML content from the URL
 page <- read_html(url)
@@ -15,10 +15,10 @@ raw_data <- page %>%
   html_text()
 
 #extracting the column names
-column_names <- raw_data[1:match("Awards", raw_data)]
+column_names <- raw_data[1:match("VORP", raw_data)]
 
 #extracting the reamining elements
-data_values <- raw_data[(match("Awards", raw_data)+1):length(raw_data)]
+data_values <- raw_data[(match("VORP", raw_data)+1):length(raw_data)]
 
 # Remove the last two items because they are extra that the scrape brings in and not part of the original table
 data_values <- data_values[1:(length(data_values) - 2)]
@@ -59,5 +59,5 @@ data_values_clean <- data_values[-indices_to_remove]
 data_matrix <- matrix(data_values_clean, ncol = length(column_names), byrow = TRUE)
 
 #convert the matrix to a data.frame and assign column names
-df_per_game <- as.data.frame(data_matrix, stringsAsFactors = FALSE)
-colnames(df_per_game) <- column_names
+df_advanced <- as.data.frame(data_matrix, stringsAsFactors = FALSE)
+colnames(df_advanced) <- column_names
