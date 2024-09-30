c68ebf843a587da23f6ed0fd4ae9535d5d9c9b10
Zach Shelly <zshelly2000@gmail.com>
2024-09-30 10:39:56 -0700

    Changing the script to accomodate for the reorg with the folders structure

--- main/main.R
+++ main/main.R
@@ -4,11 +4,13 @@ library(dplyr)
 library(httr)
 library(jsonlite)
 
+# Set the working directory to the project root
+setwd("..") # Moves up one directory form 'main/ to 'repo/'
 
 # Source the scripts
-source("Accessing_NBA_Data_via_an_API.R")
-source("Web_Scrape_Data_From_Basketball_reference_advanced.R")
-source("Web_Scrape_Data_From_Basketball_reference_per_game.R")
+source("data_gathering/Accessing_NBA_Data_via_an_API.R")
+source("data_gathering/Web_Scrape_Data_From_Basketball_reference_advanced.R")
+source("data_gathering/Web_Scrape_Data_From_Basketball_reference_per_game.R")
 
 # Call the functions
 nba_stats <- get_NBA_data_via_API()
